49865d8c29d19682b0f0e04588b8373ad062abbf
L3viathan <git@l3vi.de>
2014-10-02 10:28:55 +0200

    added goal_helper to allow route to pass goal line, not only land on it

--- racesolver.py
+++ racesolver.py
@@ -8,9 +8,9 @@ from time import sleep
 # goal = (5,22)
 # board = {((0,0),(23,0)),((23,0),(23,10)),((23,10),(18,17)),((18,17),(5,17)),((5,17),(5,19)),((5,19),(9,24)),((9,24),(0,24)),((0,24),(0,0))}
 
-car = (17,3,-1,0)
-goal = ((2,19),(2,25))
-board = {((0,0),(18,0)),((18,0),(18,6)),((18,6),(5,6)),((5,6),(5,12)),((5,12),(7,15)),((7,15),(16,15)),((16,15),(16,25)),((16,25),(0,25)),((0,25),(0,0)),((0,19),(5,19))}
+# car = (17,3,-1,0)
+# goal = ((2,19),(2,25))
+# board = {((0,0),(18,0)),((18,0),(18,6)),((18,6),(5,6)),((5,6),(5,12)),((5,12),(7,15)),((7,15),(16,15)),((16,15),(16,25)),((16,25),(0,25)),((0,25),(0,0)),((0,19),(5,19))}
 
 # car = (2,1,0,1)
 # goal = ((4,1),(9,1))
@@ -24,14 +24,29 @@ def moves(car,board):
 			line = ((car[0],car[1]),(new[0],new[1]))
 			if any([line_intersection(line,edge) for edge in board]):
 				continue
+			if line_intersection(line, goal_helper):
+				if not (line_intersection(line, goal) and distance(car,middle_point(goal)) < distance(car,middle_point(goal_helper))):
+					continue
 			yield new
+
+def build_path(listofpoints):
+	return set(zip(listofpoints[:-1],listofpoints[1:]))
 
 def distance(pos1,pos2):
 	return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
 
 def linedistance(pos1,line):
-	pos2 = ((line[0][0]+line[1][0])/2,(line[0][1]+line[1][1])/2)
+	pos2 = middle_point(line)
 	return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
+
+def middle_point(line):
+	return ((line[0][0]+line[1][0])/2,(line[0][1]+line[1][1])/2)
+	
+	
+car = (4,25,0,-1)
+goal = ((0,26),(10,26))
+goal_helper = ((0,25.5),(10,25.5))
+board = build_path([(14,0),(29,4),(31,8),(31,39),(28,42),(18,44),(3,41),(0,22),(6,5),(14,0)]) | build_path([(14,9),(25,11),(25,23),(24,27),(24,35),(18,37),(11,35),(9,33),(10,20),(8,14),(14,9)])
 
 evaluation_function = linedistance
 
@@ -57,4 +72,7 @@ def turn(car,history):
 			turn(move,history + [car])
 	return shortest
 
+print("Car:",car)
+print("Goal:",goal)
+print("Board:",board)
 turn(car,[])
